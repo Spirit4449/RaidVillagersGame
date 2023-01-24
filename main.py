40e596c5bb6712991a46ecdaa6a2358de91c0ffc
Spirit <nischayp510@gmail.com>
2023-01-23 20:21:25 -0500

    Uploaded the bulk of the code to github for the first time.

---
+++ main.py
@@ -0,0 +1,29 @@
+# DISCLAIMER
+# OPEN CONSOLE FULL SCREEN FOR THE BEST EXPERIENCE
+
+
+import time
+import random
+import colorama
+from battle import battle_start
+
+
+tutorial = False
+
+
+# Tutorial
+if tutorial == True:
+    print("Welcome to Raid Villagers! Raid villagers is a trhilling text-based adventure game where you play as a ruthless raider, attacking and looting villages for gold and treasure. Your ultimate goal is to become the most powerful raider in the land by upgrading your character's stats. You will have to make strategic choices, outsmart the village defenders, and overcome dangerous obstacles to succeed. You will gain gold throughout your journey. Spend it wisely on your character stats. Now are you ready to embark on this epic journey?  Let's being!")
+
+    time.sleep(5)
+
+    name = input('Player Name: ')
+    time.sleep(0.2)
+    print('Welcome ' + name + '. Let\'s start by attacking your first village.')
+    time.sleep(2)
+
+    battle_start()
+
+else:
+    name = input('Player Name: ')
+    battle_start(True)
